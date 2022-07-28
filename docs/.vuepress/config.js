e8612510f9d44ac1c06cc142bdb0e748b6300dfe
‘zqyu’ <912250343@qq.com>
2022-07-28 19:10:49 +0800

    2022.07.28

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,30 +11,101 @@ module.exports = {
         huawei: true,
         // 暗色模式
         mode: 'dark',
+        //  主题切换
+        modePicker: false,
         // 替换头像
         authorAvatar: '/images/tx.jpg',
+        //   左侧头像
         logo: '/images/tx.jpg',
+        //  侧边栏配置
+        subSidebar: 'auto', //在所有页面中启用自动生成子侧边栏，原 sidebar 仍然兼容
         // 顶部导航配置
         nav: [{
-            text: "主页",
-            link: "/",
-            icon: "reco-home"
-        }, {
-            text: '博客',
-            link: '/myblog/',
-            icon: 'reco-category'
-        }, {
-            text: '时光轴',
-            link: '/timeline/',
-            icon: 'reco-date'
-        }],
+                text: "主页",
+                link: "/",
+                icon: "reco-home"
+            }, {
+                text: '博客',
+                link: '/myblog/',
+                icon: 'reco-category'
+            },
+            {
+                text: '时光轴',
+                link: '/timeline/',
+                icon: 'reco-date'
+            },
+            {
+                text: '实用工具',
+                icon: 'reco-suggestion',
+                items: [{
+                        "text": "免费API",
+                        "link": "https://api.sumt.cn/"
+                    }, {
+                        "text": "站长工具",
+                        "link": "https://tool.chinaz.com/"
+                    },
+                    {
+                        "text": "图片压缩",
+                        "link": "https://tinypng.com/"
+                    },
+                    {
+                        "text": "工具集合地",
+                        "link": "https://www.5cv.top/"
+                    }, {
+                        "text": "CSS3动画库",
+                        "link": "https://www.dowebok.com/98.html"
+                    }, {
+                        "text": "iconfont图标库",
+                        "link": "https://www.iconfont.cn/"
+                    },
+                ]
+            },
+            {
+                text: '催眠利器',
+                icon: 'reco-api',
+                items: [{
+                        "text": "Vue2",
+                        "link": "https://cn.vuejs.org/"
+                    }, {
+                        "text": "Vue3",
+                        "link": "https://staging-cn.vuejs.org/"
+                    },
+                    {
+                        "text": "React",
+                        "link": "https://react.docschina.org/"
+                    },
+                    {
+                        "text": "Axios",
+                        "link": "http://www.axios-js.com/"
+                    },
+                    {
+                        "text": "Uni-app",
+                        "link": "https://uniapp.dcloud.io/"
+                    }, {
+                        "text": "W3school",
+                        "link": "https://www.w3school.com.cn/"
+                    },
+                ]
+            },
+            {
+                text: '关于我',
+                link: '/aboutMe/',
+                icon: 'reco-account'
+            }
+        ],
         // 配置博客
         type: 'blog',
+        // 博客配置信息
+        blogConfig: {
+            tag: {
+                location: 3, // 在导航栏菜单中所占的位置，默认3
+                text: '标签' // 默认文案 “标签”
+            },
+        }
     },
     // 插件
     plugins: [
         // 彩带插件
-
         "ribbon-animation", {
             size: 90, // 默认数据
             opacity: 0.3, //  透明度
@@ -67,13 +138,32 @@ module.exports = {
 
         // 樱花插件
         "sakura", {
-            num: 20, // 默认数量
+            num: 30, // 默认数量
             show: true, //  是否显示
             zIndex: -1, // 层级
             img: {
                 replace: false, // false 默认图 true 换图 需要填写httpUrl地址
                 httpUrl: '...' // 绝对路径
             }
-        }
+        },
+        // 不知道什么东西
+        'cursor-effects', {
+            size: 2, // size of the particle, default: 2
+            shape: ['star' | 'circle'], // shape of the particle, default: 'star'
+            zIndex: 999999999, // z-index property of the canvas, default: 999999999
+        },
+        //  看板娘
+        "vuepress-plugin-cat",
+        // 返回顶部插件
+        "go-top",
+        // 动态标签
+        'dynamic-title',
+        {
+            showIcon: '/favicon.ico',
+            showText: '(/≧▽≦/)咦！又好了！',
+            hideIcon: '/failure.ico',
+            hideText: '(●—●)喔哟，崩溃啦！',
+            recoverTime: 2000,
+        },
     ]
 }
